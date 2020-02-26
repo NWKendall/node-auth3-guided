7ebd53ddc3277ed564c21d57215bb0a792eceb5a
nicbongo <nicbongo@protonmail.com>
2020-02-26 13:42:45 -0400

    token working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // here
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token // sends token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +41,16 @@ router.post('/login', (req, res) => {
     });
 });
 
+
+function generateToken(user) {
+
+  const payload = {
+    username: user.username,
+  }
+  const secret = process.env.JWT_SECRET || `what ever you need`
+  const options = {
+    expiresIn: "1h"
+  }
+  return jwt.sign(payload, secret, options);
+}
 module.exports = router;
